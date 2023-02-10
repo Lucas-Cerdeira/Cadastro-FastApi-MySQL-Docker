e40be860f28c0f4434ac1c32f2b8892b3e5fc4db
Lucas-Cerdeira <cerdeiralucas4121@gmail.com>
2023-02-09 23:14:31 -0300

    fixing bugs

--- def_database.py
+++ def_database.py
@@ -1,4 +1,5 @@
 from database import conexao
+import json
 
 
 def inserir_no_bd(first_name, last_name, cpf, email, dia, mes, ano):
@@ -9,42 +10,66 @@ def inserir_no_bd(first_name, last_name, cpf, email, dia, mes, ano):
             (first_name, last_name, cpf, email, data_nascimento) 
             VALUES 
             ('{first_name}', '{last_name}', '{cpf}', '{email}', '{date(ano, mes, dia).isoformat()}');""")
-            conexao.commit()
         except Exception as erro:
             return erro
         else:
-            return f'Parabéns, {first_name}, seu cadastro foi realizado com sucesso!'
-        
+            retorno = f"Parabéns, {first_name}, seu cadastro foi realizado com sucesso!"
+            return json.dumps(retorno)
+        finally:
+            conexao.commit()
 
-def ver_dados():
+
+def ver_dados(cpf):
+    if not in_on_database(cpf):
+        return "Cpf não cadastrado na base de dados."
     with conexao.cursor() as cursor:
         try:
             cursor.execute(
                 f"""SELECT id, first_name, last_name
                 FROM pessoas 
+                WHERE cpf = '{cpf}'
                 order by id;"""
             )
-            dados = cursor.fetchall()
+            dados = cursor.fetchone()
         except Exception as erro:
             return erro
         else:
-            return [registro for registro in dados]
-        
+            return dados
 
-def deletar_dado(cpf):
-     with conexao.cursor() as cursor:
+
+def in_on_database(cpf):
+    with conexao.cursor() as cursor:
+        try:
+            cursor.execute(
+                f"""SELECT id, first_name, last_name
+                FROM pessoas 
+                WHERE cpf = '{cpf}'
+                order by id;"""
+            )
+            dados = cursor.fetchone()
+            if dados == None:
+                return False
+        except Exception as erro:
+            return erro
+        else:
+            return True
+
+
+def deletar_dado(cpf):  # verificar se cpf está cadastrado
+    with conexao.cursor() as cursor:
         try:
             cursor.execute(
                 f"""DELETE FROM pessoas as p
                 WHERE cpf = '{cpf}';"""
             )
-            conexao.commit()
         except Exception as erro:
             return erro
         else:
             return 'Seus dados foram deletados com sucesso.'
-        
-    
+        finally:
+            conexao.commit()
+
+
 def update_data(cpf, campo_alterado, novo_registro):
     with conexao.cursor() as cursor:
         try:
@@ -53,8 +78,9 @@ def update_data(cpf, campo_alterado, novo_registro):
                 SET {campo_alterado} = '{novo_registro}' 
                 WHERE cpf = '{cpf}';"""
             )
-            conexao.commit()
         except Exception as erro:
             return erro
         else:
             return 'Alteração realizada com sucesso!'
+        finally:
+            conexao.commit()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from database import conexao
-from def_database import inserir_no_bd, ver_dados, deletar_dado, update_data
+from def_database import inserir_no_bd, ver_dados, deletar_dado, update_data, in_on_database
 from datetime import date
 from utils import validate_cpf, valid_email
 import json
@@ -11,37 +11,42 @@ app = FastAPI()
 
 @app.get("/Inserir_Pessoa")
 def cadastro_pessoa(first_name: str, last_name: str, cpf: str, email: str, dia: int, mes: int, ano: int):
+    if not in_on_database(cpf):
+        return json.dumps("Cpf não cadastrado na base de dados.", ensure_ascii=False)
     if not validate_cpf(cpf):
-        retorno = 'Por favor insira um cpf válido.'
-        return retorno
+        return json.dumps("Por favor insira um cpf válido.", ensure_ascii=False)
     if not valid_email(email):
-        retorno = 'Por favor insira um e-mail válido.'
-        return retorno
+        return json.dumps("Por favor insira um e-mail válido.")
     retorno = inserir_no_bd(first_name, last_name, cpf, email, dia, mes, ano)
-    return retorno
+    return json.dumps(retorno)
 
 
-@app.get("Ver_pessoas")
-def ver_pessoas():
-    dados = ver_dados()
-    return dados
+@app.get("/Ver_pessoas")
+def ver_pessoas(cpf):
+    if not in_on_database(cpf):
+        return json.dumps("Cpf não cadastrado na base de dados.", ensure_ascii=False)
+    if not validate_cpf(cpf):
+        return json.dumps("Por favor insira um cpf válido.", ensure_ascii=False)
+    return json.dumps(ver_dados(cpf))
 
 
 @app.delete("/Remover_Pessoa")
 def deletar(cpf):
+    if not in_on_database(cpf):
+        return json.dumps("Cpf não cadastrado na base de dados.", ensure_ascii=False)
     if not validate_cpf(cpf):
-        retorno = 'Por favor insira um cpf válido.'
-        return retorno
+        return json.dumps("Por favor insira um cpf válido.", ensure_ascii=False)
     retorno = deletar_dado(cpf)
-    return retorno
+    return json.dumps(retorno)
 
 
 @app.put("/att_info")
 def atualiza_info(cpf: str, campo_alterado: str, novo_registro: str):
+    if not in_on_database(cpf):
+        return json.dumps("Cpf não cadastrado na base de dados.", ensure_ascii=False)
     if not validate_cpf(cpf):
-        retorno = 'Por favor insira um cpf válido.'
-        return retorno
+        return json.dumps("Por favor insira um cpf válido.", ensure_ascii=False)
     if not campo_alterado in ['email', 'first_name']:
         return False
     retorno = update_data(cpf, campo_alterado, novo_registro)
-    return retorno
+    return json.dumps(retorno, ensure_ascii=False)
